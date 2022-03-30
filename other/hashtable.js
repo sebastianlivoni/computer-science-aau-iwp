4d1828372cd549fb8dc6dad699b789d66e23c0f7
Sebastian Livoni <seb@livoni.me>
2022-03-30 11:30:38 +0200

    added hashtable calculator

---
+++ other/hashtable.js
@@ -0,0 +1,64 @@
+const m = 11
+const c1 = 1
+const c2 = 3
+
+const arr = [10, 22, 31, 4, 15, 28, 17, 88, 59]
+
+function linearHashing(arr) {
+  const h = (k) => k
+
+  let hashtable = []
+
+  arr.forEach((k) => {
+    let i = 0
+    let hash = (h(k) + i) % m
+    while (hashtable[hash]) {
+      hash++
+      //hash = (h(k) + i) % m
+    }
+    hashtable[hash] = k
+  })
+
+  return hashtable
+}
+
+function quadraticHashing(arr) {
+  const h = (k) => k
+
+  let hashtable = []
+
+  arr.forEach((k) => {
+    let i = 0
+    let hash = (h(k) + c1 * i + c2 * Math.pow(i, 2)) % m
+    while (hashtable[hash]) {
+      hash++
+      //hash = (h(k) + c1 * i + c2 * Math.pow(i, 2)) % m
+    }
+    hashtable[hash] = k
+  })
+
+  return hashtable
+}
+
+function doubleHashing(arr) {
+  const h1 = (k) => k
+  const h2 = (k) => k % (m - 1)
+
+  let hashtable = []
+
+  arr.forEach((k) => {
+    let i = 0
+    let hash = (h1(k) * k + i * h2(k)) % m
+    while (hashtable[hash]) {
+      hash++
+      //hash = (h1(k) * k + i * h2(k)) % m
+    }
+    hashtable[hash] = k
+  })
+
+  return hashtable
+}
+
+console.log('Linear Hashing: ', linearHashing(arr))
+console.log('Quadratic Hashing: ', quadraticHashing(arr))
+console.log('Double Hashing: ', doubleHashing(arr))
